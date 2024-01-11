a267e568f2916580bacabb4279e91abe0f0d3069
Danijurado <danieljp.85@gmail.com>
2024-01-11 23:11:21 +0100

    view int

--- controllers/PrivadoController.js
+++ controllers/PrivadoController.js
@@ -1,7 +1,23 @@
+const User = require('../models/User');
+var createError = require("http-errors");
+
 class PrivadoController {
-    index(req, res, next) {
-       
-        res.render('privado');
+    async index(req, res, next) {
+        try {
+            const userId = req.session.usuarioLogado;
+            
+            const user = await User.findById(userId);
+            
+            if(!user) {
+                next(createError(500, 'usuario not found'))
+                return;
+            }
+            res.render('privado', {email: user.email});
+        } catch (error) {
+            next(error)
+        }
+
+
     }
 }
 
